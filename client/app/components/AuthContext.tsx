582637e00f992e4d10f7d8de45ef9b2e4ae77701
Mahikagarg09 <mg@gwdcollective.com>
2024-05-05 01:52:03 +0530

    socket.io install

--- client/app/components/AuthContext.tsx
+++ client/app/components/AuthContext.tsx
@@ -1,6 +1,9 @@
 "use client";
 import { createContext, useContext, useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
+import io from 'socket.io-client';
+
+const socket = io('http://localhost:5500');
 
 const AuthContext = createContext(null);
 
